1e8f54738dc9e71cf4cea2650b683c0f9f61dfe2
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-26 14:48:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ for (let x= 0; x < 700; x++) {
 }
 console.log (Array)
 
+let array3 = [];
+for (let f=0; f<500; f++ ){
+    array3.push(7)
+}
+console.log(array3)
+
+let array2 = [];
+for(let q=0; q<5000; q++){
+    array2.push(Math.random()*100)
+}
+//console.log (array2)
+
+let arrry4 = [];
+for(let k=0; k<300; k++) {
+    arrry4.push(Math.random()*40)
+}
+console.log(arrry4)
