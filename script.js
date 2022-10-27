8200e33836d476ae49548595f60d430fd92e4293
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2022-10-27 12:03:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-let  months = ["jan", "june", "july"]
-console.log(months)
+let months = ["jan", "june", "july"];
+console.log(months);
 
 let Array = [];
-for (let x= 0; x < 700; x++) {
-    Array.push("joy");
+for (let x = 0; x < 700; x++) {
+  Array.push("joy");
 }
-console.log (Array)
+console.log(Array);
 
 let array3 = [];
-for (let f=0; f<500; f++ ){
-    array3.push(7)
+for (let f = 0; f < 500; f++) {
+  array3.push(7);
 }
-console.log(array3)
+console.log(array3);
 
 let array2 = [];
-for(let q=0; q<5000; q++){
-    array2.push(Math.random()*100)
+for (let q = 0; q < 5000; q++) {
+  array2.push(Math.random() * 100);
 }
 //console.log (array2)
 
 let arrry4 = [];
-for(let k=0; k<300; k++) {
-    arrry4.push(Math.random()*40)
+for (let k = 0; k < 300; k++) {
+  arrry4.push(Math.random() * 40);
 }
-console.log(arrry4)
+console.log(arrry4);
+
+let array5 = [];
+for (let z = 20; z < 804; z += 4) {
+  array5.push(z);
+}
+console.log(array5);
+
+let array6 = [];
+for (let x = 20; x < 100; x -= 2) {
+    array6.push(x);
+}
+console.log(array6)
